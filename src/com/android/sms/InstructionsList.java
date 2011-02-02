2686e73460ae88e0dd129656df3184f895a1c85a
kwan26 <kwan@ubuntu>
2011-02-02 16:08:00 +0000

    added listview to path

--- src/com/android/sms/InstructionsList.java
+++ src/com/android/sms/InstructionsList.java
@@ -8,48 +8,72 @@ import java.util.List;
 
 
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.jsambells.directions.StepAbstract;
+import com.jsambells.directions.LegAbstract;
 import com.jsambells.directions.google.DirectionsAPI;
 import com.jsambells.directions.google.DirectionsAPIStep;
+import com.jsambells.directions.google.DirectionsAPILeg;
 import com.jsambells.directions.google.teste;
 
 
-public class InstructionsList extends ListActivity {
+public class InstructionsList extends Activity {
 	
 	ListView lv1;
+	TextView tv1;
 	
 	@Override
-	protected void onCreate(android.os.Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.list_item);
 
-	
+		lv1 = (ListView)findViewById(R.id.listview);
+		tv1 = (TextView)findViewById(R.id.tvLocation);
+		
+		
 		 List<DirectionsAPIStep> allPharm = teste.getInstructions();
 	
+		 DirectionsAPILeg a = new DirectionsAPILeg();
+		 
+		 tv1.setText(a.getEndAddress());
+		 
 		 ArrayList<HashMap<String, String>> pharmacies = new ArrayList<HashMap<String, String>>();
 			for (DirectionsAPIStep p : allPharm) {
 				HashMap<String, String> pharmacy = new HashMap<String, String>();
-				pharmacy.put("name", p.getDistance());
+			//	pharmacy.put("name", p.getDistance());
+				
 				pharmacy.put("instructions", Html.fromHtml(p.getInstructions()).toString());
-				Log.d("dfedf",p.getInstructions());
+				//Log.d("dfedf",p.getInstructions());
 				pharmacies.add(pharmacy);
 			}
 
 			
-			SimpleAdapter adapter = new SimpleAdapter(this, pharmacies, R.layout.list_item,
+			String[] from = new String[] {"instructions"};
+			int[] to = new int[] { R.id.txtTexto};
+			
+			  SimpleAdapter adapter = new SimpleAdapter(this, pharmacies, R.layout.list_row, from, to);
+	
+			          lv1.setAdapter(adapter);
+
+			
+			/*SimpleAdapter adapter = new SimpleAdapter(this, pharmacies, R.layout.list_item,
 	                new String[] {"instructions"},
 	                new int[] {R.id.txtTexto}
-	        );
+	        );*/
 
-	setListAdapter(adapter);
+	//setListAdapter(adapter);
+			
+		//	lv1.setAdapter(adapter);
 			//Log.d("ahaha",pharmacies.toString());
 		
 		//Log.d("aaaaaaaaaa",instruct.getInstructions().toString());
--- src/com/android/sms/Sms.java
+++ src/com/android/sms/Sms.java
@@ -61,13 +61,16 @@ public class Sms extends Activity {
 				if(phonen.length() > 0 && phoneMessage.length() > 0){
 					sendSms(phonen,phoneMessage);
 				}else{
-				//	Toast.makeText(getApplicationContext(), "Insira o numero e a mensagem", Toast.LENGTH_LONG).show();
-					  Intent i = new Intent(Sms.this, com.android.sms.maps.class);
-			           startActivity(i);
+					Toast.makeText(getApplicationContext(), "Insira o numero ", Toast.LENGTH_LONG).show();
+					 // Intent i = new Intent(Sms.this, com.android.sms.maps.class);
+			           //startActivity(i);
 				}
 				
 			}
 		});
+        
+        
+        //Query à base de dados dos contactos
         
         ContentResolver content = getContentResolver();
         Cursor cursor = content.query(ContactsContract.Contacts.CONTENT_URI,
