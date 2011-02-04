05705e062396a3fc520e30269a0d567a228354f5
kwan26 <kwan@ubuntu>
2011-02-04 18:30:36 +0000

    changes on main.xml

--- gen/com/android/sms/R.java
+++ gen/com/android/sms/R.java
@@ -23,15 +23,14 @@ public final class R {
         public static final int TableRow03=0x7f05000d;
         public static final int TextView01=0x7f050008;
         public static final int TextView02=0x7f05000b;
-        public static final int btnSendSMS=0x7f050011;
+        public static final int btnSendSMS=0x7f050010;
         public static final int ivStatus=0x7f050007;
         public static final int listview=0x7f050004;
-        public static final int main=0x7f050013;
-        public static final int mapview=0x7f050012;
+        public static final int main=0x7f050012;
+        public static final int mapview=0x7f050011;
         public static final int tvDistancia=0x7f05000e;
         public static final int tvDuracao=0x7f05000c;
         public static final int tvLocation=0x7f050003;
-        public static final int txtMessage=0x7f050010;
         public static final int txtPhoneNo=0x7f05000f;
         public static final int txtTexto=0x7f050009;
     }
